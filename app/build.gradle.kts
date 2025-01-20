2c2ad1691e59b4109880e791e201ea66b59eeb68
NSC-david-sasaki <david.sasaki@seattlecolleges.edu>
2025-01-20 03:24:37 +0000

    add T&C for gradle fix syntax in gradle for plugin java

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,8 +7,8 @@
 
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
-    application
-    id 'java'
+    application;
+    id("java")
 }
 
 repositories {
